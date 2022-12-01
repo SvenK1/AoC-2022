e98d5131764796c0850866a5ebc478dc5871f133
Sven K <svenk@delta-n.nl>
2022-12-01 11:29:28 +0100

    Changes

---
+++ Advent of Code/Advent of Code/Dag 1/Calories.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code.Dag_1
+{
+    public class Calories
+    {
+        public static async Task<int> MostCalories()
+        {
+
+            string text = await File.ReadAllTextAsync("C:\\Users\\svenk\\AoC 2022\\Advent of Code\\Advent of Code\\Dag 1\\input.txt");
+            int calories = 0;
+            int varCalories;
+            int maxCalories = 0;
+
+            foreach (var s in text.Split("\n"))
+            {
+                int.TryParse(s.Trim(), out varCalories);
+                calories += varCalories;
+                if (string.IsNullOrEmpty(s.Trim()))
+                {
+                    if (maxCalories < calories)
+                         maxCalories = calories;
+                       
+                    calories = 0;
+                }
+            }
+            return maxCalories;
+        }
+    }
+}
---
+++ Advent of Code/Advent of Code/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Advent_of_Code.Dag_1;
+
+Console.WriteLine(Calories.MostCalories().Result);
